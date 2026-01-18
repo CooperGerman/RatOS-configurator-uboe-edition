33a314b71f29c12844524d0dacc08c01fb836651
Brian Vanderbusch <chief@hackerhappyhour.com>
2026-01-18 15:04:51 -0600

    fix: don't want to delete local git history

--- scripts/create-local-deployment.sh
+++ scripts/create-local-deployment.sh
@@ -210,7 +210,7 @@ create_deployment_branch(){
     fi
 
     # unpack the artifacts build directory into the current working directory
-    rsync -a --delete "$(get_artifact_build_directory)/" ./
+    rsync -a --delete --exclude=".git" "$(get_artifact_build_directory)/" ./
     echo "Deployment branch ${deployment_branch} is ready with the latest build artifacts."
 }
 
